a9d3ae5fb995c8647e3789c73229b63e20f5015a
Michal <mekalissa0@gmail.com>
2020-03-18 13:09:11 +0200

    test

--- app/routes/main.py
+++ app/routes/main.py
@@ -57,7 +57,7 @@ def ipn():
     if r.text == 'VERIFIED':
 
         payer_email =  request.form.get('payer_email')
-        unix = datetime.utcnow
+        unix = 122123
         payment_date = request.form.get('payment_date')
         username = request.form.get('first_name')
         last_name = request.form.get('last_name')
