41c6f187ff185cde3f313c7270e6cd9e7d4de356
Michal <mekalissa0@gmail.com>
2020-03-18 11:29:26 +0200

    initial commit

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from .extensions import db
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unix = db.Column(db.Integer)
+    payment_date = db.Column(db.String(400))
+    username = db.Column(db.String(400))
+    last_name = db.Column(db.String(400))
+    payment_gross = db.Column(db.Float(6,2))
+    payment_fee = db.Column(db.Float(6,2))
+    payment_net = db.Column(db.Float(6,2))
+    payment_status = db.Column(db.String(400))
+    txn_id = db.Column(db.String(100))
+
+   
+
+    def __repr__(self):
+        return f"Reply('{self.username}', '{self.last_name}')"
---
+++ app/routes/main.py
@@ -0,0 +1,83 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+
+
+from app.extensions import db
+
+from app.models import Payment
+
+
+main = Blueprint('main', __name__)
+
+
+
+@main.route('/')
+def purchase():
+	try:
+		return render_template("subscribe.html")
+	except Exception as e:
+		return(str(e))
+
+
+
+@main.route('/success/')
+def success():
+	try:
+		return render_template("success.html")
+	except Exception as e:
+		return(str(e))
+
+
+
+
+@main.route('/ipn', methods=['GET','POST'])
+def ipn():
+
+    arg = ''
+    request.parameter_storage_class = ImmutableOrderedMultiDict
+    values = request.form
+
+
+    for x, y in values.items():
+        arg += "&{x}={y}".format(x=x,y=y)
+
+
+    validate_url = 'https://www.sandbox.paypal.com' \
+					   '/cgi-bin/webscr?cmd=_notify-validate{arg}' \
+					   .format(arg=arg)
+
+    r = requests.get(validate_url)
+
+    if r.text == 'VERIFIED':
+
+        payer_email =  request.form.get('payer_email')
+        unix = int(time.time())
+        payment_date = request.form.get('payment_date')
+        username = request.form.get('custom')
+        last_name = request.form.get('last_name')
+        payment_gross = request.form.get('payment_gross')
+        payment_fee = request.form.get('payment_fee')
+        payment_net = float(payment_gross) - float(payment_fee)
+        payment_status = request.form.get('payment_status')
+        txn_id = request.form.get('txn_id')
+        print('##########################', payment_status)
+
+        payment = Payment(unix=unix, payment_date=payment_date, username=username, last_name=last_name, payment_gross=payment_gross, payment_fee=payment_fee, payment_net=payment_net, payment_status=payment_status, txn_id=txn_id)
+        posts_db.session.add(payment)
+        posts_db.session.commit()
+
+
+
+        with open('/tmp/ipnout.txt','a') as f:
+            data = 'SUCCESS\n'+str(values)+'\n'
+            f.write(data)
+
+        
+
+        
+
+    else:
+        with open('/tmp/ipnout.txt','a') as f:
+            data = 'FAILURE\n'+str(values)+'\n'
+            f.write(data)
+
+    return r.text
