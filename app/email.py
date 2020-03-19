36a8276dd533b3e93af35bdc7f5b9547fedbb7ce
Michal <mekalissa0@gmail.com>
2020-03-19 10:47:07 +0200

    test

--- app/email.py
+++ app/email.py
@@ -1,12 +1,12 @@
 from threading import Thread
 from flask import current_app
 from flask_mail import Message
-import mail
+import app
 
 
 def send_async_email(app, msg):
     with app.app_context():
-        mail.send(msg)
+        app.mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body):
